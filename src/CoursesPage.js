915f6f4724dabf09afcf445eef9096e763f09213
Cory House <housecor@gmail.com>
2019-05-09 12:10:05 -0500

    Push changes

--- src/CoursesPage.js
+++ src/CoursesPage.js
@@ -1,32 +1,72 @@
 import React from "react";
 
 class CoursePage extends React.Component {
-  renderCourseRow(course) {
-    return (
-      <tr key={course.id}>
-        <td>{course.title}</td>
-        <td>{course.category}</td>
-      </tr>
-    );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      courses: [
+        { id: 1, title: "Horses for dummies", category: "Pets" },
+        { id: 2, title: "Clean code", category: "Programming" }
+      ],
+      isAdmin: false
+    };
+
+    // Binding in the constructor
+    // this.handleDelete = this.handleDelete.bind(this);
   }
 
-  render() {
-    const courses = [
-      { id: 1, title: "Horses for dummies", category: "Pets" },
-      { id: 2, title: "Clean code", category: "Programming" }
-    ];
+  handleDelete = event => {
+    const idToDelete = event.target.name;
 
+    // Option 1: Object assign
+    // const coursesCopy = Object.assign([], this.state.courses);
+
+    // Option 2: Object spread
+    const courseCopy = [...this.state.courses];
+
+    // Option 3: Just use filter.
+    const courses = this.state.courses.filter(
+      course => course.id !== parseInt(idToDelete)
+    );
+
+    debugger;
+    // Goal: New courses array, that omits the course with the passed id.
+
+    // this.setState({ courses: })
+
+    // I need to know what course id to delete. How do I get that?
+  };
+
+  render() {
     return (
       <>
         <h1>Courses</h1>
         <table>
           <thead>
             <tr>
+              <th />
               <th>Title</th>
               <th>Category</th>
             </tr>
           </thead>
-          <tbody>{courses.map(this.renderCourseRow)}</tbody>
+          <tbody>
+            {this.state.courses.map(course => (
+              <tr key={course.id}>
+                <td>
+                  <button
+                    onClick={this.handleDelete}
+                    name={course.id}
+                    type="button"
+                  >
+                    Delete
+                  </button>
+                </td>
+                <td>{course.title}</td>
+                <td>{course.category}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </>
     );
